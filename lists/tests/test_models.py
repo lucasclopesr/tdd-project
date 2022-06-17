56510221744001bd1a3991c95ab7e4406799bb38
Lucas Caetano <lucasclopesr@gmail.com>
2022-06-17 16:47:49 -0300

    Validação: Prevenindo itens vazios - PARTE 2

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 class ListAndItemModelTest(TestCase):
 
@@ -33,3 +34,10 @@ class ListAndItemModelTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
